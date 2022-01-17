47ac84d068d71137c68a4241fb123c704af6b113
SangHangNing <664652740@qq.com>
2022-01-17 22:17:53 +0800

    pref:sidebar

--- src/components/layout/frame/Menu.vue
+++ src/components/layout/frame/Menu.vue
@@ -3,20 +3,24 @@
     <template v-for="(item1,index1) in list" :key="`meun-lv1-${index1}`">
       <a-menu-item :key="item1.name" v-if="!(item1.child && item1.child.length > 0)">
         <component :is="item1.icon" />
-        {{item1.label}}
+        <span>{{item1.label}}</span>
       </a-menu-item>
       <template v-else>
         <a-sub-menu :key="item1.name">
           <template #title>
             <component :is="item1.icon" />
-            {{item1.label}}
+            <span>{{item1.label}}</span>
           </template>
           <template v-for="(item2,index2) in item1.child" :key="`meun-lv2-${index2}`">
             <a-menu-item :key="item2.name" v-if="!(item2.child && item2.child.length > 0)">{{item2.label}}</a-menu-item>
             <template v-else>
               <a-sub-menu :key="item2.name">
-                <template #title>{{item2.label}}</template>
-                <a-menu-item v-for="(item3) in item2.child" :key="item3.name">{{item3.label}}</a-menu-item>
+                <template #title>
+                  <span>{{item2.label}}</span>
+                </template>
+                <a-menu-item v-for="(item3) in item2.child" :key="item3.name">
+                  <span>{{item3.label}}</span>
+                </a-menu-item>
               </a-sub-menu>
             </template>
           </template>
