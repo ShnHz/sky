400de4131aa81c37492a7f0b0d8bac45a5996cec
sanghangning <wb.sanghangning@mesg.corp.netease.com>
2022-01-24 18:00:27 +0800

    config:update router

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_NAME),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // 始终滚动到顶部
