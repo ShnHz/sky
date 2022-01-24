741e907c8a164dbd8e70bb4a584a206f285f7c18
sanghangning <wb.sanghangning@mesg.corp.netease.com>
2022-01-24 17:26:37 +0800

    config:update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {
-  defineConfig
+  defineConfig,
+  loadEnv
 } from 'vite'
 import path from "path";
 
@@ -9,37 +10,41 @@ import {
 } from './src/assets/svg/svgBuilder';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/vue-admin/',
-  build: {
-    outDir: 'docs'
-  },
-  plugins: [vue(), svgBuilder('./src/assets/svg/svg/')],
-  server: {
-    proxy: {
-      '/devapi': '0.0.0.0'
-    }
-  },
-  resolve: {
-    // 文件夹别名
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@static": path.resolve(__dirname, "public/static"),
-      "@img": path.resolve(__dirname, "src/assets/img"),
-      "@css": path.resolve(__dirname, "src/assets/css"),
+export default ({
+  mode
+}) => {
+  return defineConfig({
+    base: loadEnv(mode, process.cwd()).VITE_APP_NAME,
+    build: {
+      outDir: 'docs'
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@css/varuables.scss";`
+    plugins: [vue(), svgBuilder('./src/assets/svg/svg/')],
+    server: {
+      proxy: {
+        '/devapi': '0.0.0.0'
+      }
+    },
+    resolve: {
+      // 文件夹别名
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        "@static": path.resolve(__dirname, "public/static"),
+        "@img": path.resolve(__dirname, "src/assets/img"),
+        "@css": path.resolve(__dirname, "src/assets/css"),
       },
-      less: {
-        modifyVars: {
-          'arcoblue-6': '#1d63ff',
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@css/varuables.scss";`
         },
-        javascriptEnabled: true,
+        less: {
+          modifyVars: {
+            'arcoblue-6': '#1d63ff',
+          },
+          javascriptEnabled: true,
+        }
       }
     }
-  }
-})
+  })
+}
