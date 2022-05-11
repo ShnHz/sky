eda090eaa6b7fdc5239cc1db8037a07778898f27
sanghangning <wb.sanghangning@mesg.corp.netease.com>
2022-05-11 15:55:59 +0800

    feat:component:Image

---
+++ src/components/common/Image.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-image :width="width" :height="height" :src="src" :preview="false"/>
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: [String, Number],
+      default: 'auto',
+    },
+    height: {
+      type: [String, Number],
+      default: 'auto',
+    },
+
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="scss" scoped>
+</style>
--- src/components/components.js
+++ src/components/components.js
@@ -2,10 +2,11 @@
  * @Author: sanghangning 
  * @Date: 2019-12-11 10:48:06 
  * @Last Modified by: sanghangning
- * @Last Modified time: 2022-02-11 12:10:13
+ * @Last Modified time: 2022-05-11 15:47:40
  */
 
 import Empty from '@/components/common/Empty.vue'
+import Image from '@/components/common/Image.vue'
 import UrlParam from '@/components/common/UrlParam.vue'
 
 import SubTitleWrap from '@/components/layout/SubTitleWrap.vue'
@@ -13,10 +14,11 @@ import SubTitleWrap from '@/components/layout/SubTitleWrap.vue'
 import PluginsSvgIcon from '@/components/plugins/PluginsSvgIcon.vue'
 
 const components = {
-    'Empty': Empty,
-    'UrlParam': UrlParam,
-    'SubTitleWrap': SubTitleWrap,
-    'PluginsSvgIcon': PluginsSvgIcon,
+    Empty,
+    Image,
+    UrlParam,
+    SubTitleWrap,
+    PluginsSvgIcon,
 }
 
 export function loadAllComponents(app) {
